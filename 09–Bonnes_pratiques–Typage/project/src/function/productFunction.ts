1cbe3307094d613ef8c5197df1ffd775af4df528
CorentinKSTR <Corentin.kistler@edu.devinci.fr>
2024-07-30 11:25:41 +0200

    :construction: update Functions

--- 09–Bonnes_pratiques–Typage/project/src/function/productFunction.ts
+++ 09–Bonnes_pratiques–Typage/project/src/function/productFunction.ts
@@ -1,7 +1,7 @@
 import { Product } from '../types/Product';
 
-let products: Product[] = [];
 
+let products: Product[] = [];
 
 export const addProduct = (product: Product): void => {
   products.push(product);
@@ -13,4 +13,11 @@ export const getProducts = (): Product[] => {
 
 export const removeProduct = (productId: string): void => {
   products = products.filter(product => product.id !== productId);
+};
+
+export const updateProductQuantity = (productId: string, quantity: number): void => {
+  const product = products.find(product => product.id === productId);
+  if (product) {
+    product.quantity = quantity;
+  }
 };
